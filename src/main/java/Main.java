36eb4c5e95a15bcfb67848f4ad222b548d78e7d0
adriansd1 <154755212+adriansd1@users.noreply.github.com>
2024-02-04 02:09:36 +0000

    login method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,19 @@ public class Main
   {
 
     System.out.println("Hello world! Armin sup");
+
+
+
+    if(login("test", "test")){
+      print("hey there");
+    }
+  }
+
+
+  private static boolean login(String username, String password){
+    if(username.equals("test") && password.equals("test")){
+      return true;
+    }
+    else{return false;}
   }
 }
