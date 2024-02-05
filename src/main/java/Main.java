0e709e0518618b6b6dfa134e7dfd3fa791d756e5
Armin971 <158786127+Armin971@users.noreply.github.com>
2024-02-05 04:24:14 +0000

    menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 import java.lang.Thread;
 
 public class Main 
 {
   public static void main(String[] args) 
   {
+    Scanner input = new Scanner(System.in);
+
+    int userOption;
+    
     loading();
     if(login("test", "test"))
     {
@@ -18,19 +23,65 @@ public class Main
       print("6) Guessing Game");
       print("7) OS Version");
       print("8) Exit");
+      userOption = input.nextInt();
+      
+      if(userOption == 1)
+      {
+        System.out.print("\033[H\033[2J" + "Which Math Operation Would You Like to Use?" + "\n" + "1) Addition" + "\n" + "2) Subtraction" + "\n" + "3) Multiplication" + "\n" + "4) Division" + "\n" + "5) Exponents" + "\n" + "6) Square Roots" + "\n");
+      }
+      else if(userOption == 2)
+      {
+        System.out.print("\033[H\033[2J" + "You chose AI ChatBot");
+      }
+
+      else if(userOption == 3)
+      {
+        System.out.print("\033[H\033[2J" + "You chose Roll the DIce");
+      }
+
+      else if(userOption == 4)
+      {
+        System.out.print("\033[H\033[2J" + "You chose Meme Generator");
+      }
+
+      else if(userOption == 5)
+      {
+        System.out.print("\033[H\033[2J" + "You chose Stoic Quote");
+      }
+
+      else if(userOption == 6)
+      {
+        System.out.print("\033[H\033[2J" + "You chose Guessing Game");
+      }
+
+      else if(userOption == 7)
+      {
+        System.out.print("\033[H\033[2J" + "You chose OS Version");
+      }
+
+      else if(userOption == 8)
+      {
+        System.out.print("\033[H\033[2J" + "You chose Exit");
+      }
+      else
+      {
+        System.out.println("Invalid Option, Please Try Again");
+      }
+      
     }
- 
-    
+      
   }
+          
   private static void print(String text)
   {
     System.out.println(text);
 
   }
 
-
-  private static boolean login(String username, String password){
-    if(username.equals("test") && password.equals("test")){
+  private static boolean login(String username, String password)
+  {
+    if(username.equals("test") && password.equals("test"))
+    {
       return true;
     }
     else
@@ -39,8 +90,10 @@ public class Main
     }
   }
   private static void loading(){
-    try {
-        for (int j = 0; j < 10; j++) {
+    try 
+      {
+        for (int j = 0; j < 10; j++) 
+        {
           System.out.print("\033[H\033[2J");
           System.out.print("Loading");
           System.out.print(".");
@@ -50,19 +103,22 @@ public class Main
           System.out.print(".");
           Thread.sleep(300);
         }
-      for(int i = 0; i < 100; i++){
+      for(int i = 0; i < 100; i++)
+      {
         System.out.print("\033[H\033[2J");
         //System.out.println("Loading...");
         System.out.println("\rProgess: " + i + "%");
         Thread.sleep(50);
       }
       Thread.sleep(200);
-    } catch (InterruptedException e) {
+    } catch (InterruptedException e) 
+      {
         Thread.currentThread().interrupt();
     }
   }
 }
-class userInfo{
+class userInfo
+{
   String username;
   String password;
 }
